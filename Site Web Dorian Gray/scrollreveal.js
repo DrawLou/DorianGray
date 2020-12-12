b10c1f384c763c122e47e57f6f1388537c14a486
DrawLou <74010593+DrawLou@users.noreply.github.com>
2020-12-12 18:48:24 +0100

    ajout js

---
+++ Site Web Dorian Gray/scrollreveal.js
@@ -0,0 +1,148 @@
+$(function() {
+
+    var sr=ScrollReveal({
+        duration: 2000,
+        reset: true
+    });
+
+ScrollReveal().reveal('#blocImage1', {
+    duration: 1500,     
+    origin: 'bottom',   
+    distance: '50px',   
+    scale: 0.2,         
+    opacity: 0,         
+    easing: 'cubic-bezier(0.6, 0.4, 0.1, 1)',   
+    rotate: {x:0, y:0, z:0}, 
+    delay: 500, 
+});
+
+ScrollReveal().reveal('#text1', {
+    duration: 1500,     
+    origin: 'bottom',   
+    distance: '50px',   
+    scale: 0.2,         
+    opacity: 0,         
+    easing: 'cubic-bezier(0.6, 0.4, 0.1, 1)',   
+    rotate: {x:0, y:0, z:0}, 
+    delay: 500, 
+});
+
+ScrollReveal().reveal('#blocImage2', {
+    duration: 1500,     
+    origin: 'bottom',   
+    distance: '50px',   
+    scale: 0.2,         
+    opacity: 0,         
+    easing: 'cubic-bezier(0.6, 0.4, 0.1, 1)',   
+    rotate: {x:0, y:0, z:0}, 
+    delay: 500, 
+
+ScrollReveal().reveal('#text2', {
+    duration: 1500,     
+    origin: 'bottom',   
+    distance: '50px',   
+    scale: 0.2,         
+    opacity: 0,         
+    easing: 'cubic-bezier(0.6, 0.4, 0.1, 1)',   
+    rotate: {x:0, y:0, z:0}, 
+    delay: 500, 
+});
+
+ScrollReveal().reveal('#blocImage3', {
+    duration: 1500,     
+    origin: 'bottom',   
+    distance: '50px',   
+    scale: 0.2,         
+    opacity: 0,         
+    easing: 'cubic-bezier(0.6, 0.4, 0.1, 1)',   
+    rotate: {x:0, y:0, z:0}, 
+    delay: 500, 
+
+ScrollReveal().reveal('#text3', {
+    duration: 1500,     
+    origin: 'bottom',   
+    distance: '50px',   
+    scale: 0.2,         
+    opacity: 0,         
+    easing: 'cubic-bezier(0.6, 0.4, 0.1, 1)',   
+    rotate: {x:0, y:0, z:0}, 
+    delay: 500, 
+});
+
+ScrollReveal().reveal('.portrait3 h2', {
+    duration: 1500,     
+    origin: 'bottom',   
+    distance: '50px',   
+    scale: 0.2,         
+    opacity: 0,         
+    easing: 'cubic-bezier(0.6, 0.4, 0.1, 1)',   
+    rotate: {x:0, y:0, z:0}, 
+    delay: 500, 
+});
+
+ScrollReveal().reveal('#blocImage4', {
+    duration: 1500,     
+    origin: 'bottom',   
+    distance: '50px',   
+    scale: 0.2,         
+    opacity: 0,         
+    easing: 'cubic-bezier(0.6, 0.4, 0.1, 1)',   
+    rotate: {x:0, y:0, z:0}, 
+    delay: 500, 
+});
+
+ScrollReveal().reveal('#text4', {
+    duration: 1500,     
+    origin: 'bottom',   
+    distance: '50px',   
+    scale: 0.2,         
+    opacity: 0,         
+    easing: 'cubic-bezier(0.6, 0.4, 0.1, 1)',   
+    rotate: {x:0, y:0, z:0}, 
+    delay: 500, 
+});
+
+ScrollReveal().reveal('.portrait4 h2', {
+    duration: 1500,     
+    origin: 'bottom',   
+    distance: '50px',   
+    scale: 0.2,         
+    opacity: 0,         
+    easing: 'cubic-bezier(0.6, 0.4, 0.1, 1)',   
+    rotate: {x:0, y:0, z:0}, 
+    delay: 500, 
+});
+
+ScrollReveal().reveal('#blocImage5', {
+    duration: 1500,     
+    origin: 'bottom',   
+    distance: '50px',   
+    scale: 0.2,         
+    opacity: 0,         
+    easing: 'cubic-bezier(0.6, 0.4, 0.1, 1)',   
+    rotate: {x:0, y:0, z:0}, 
+    delay: 500, 
+});
+
+ScrollReveal().reveal('#text5', {
+    duration: 1500,     
+    origin: 'bottom',   
+    distance: '50px',   
+    scale: 0.2,         
+    opacity: 0,         
+    easing: 'cubic-bezier(0.6, 0.4, 0.1, 1)',   
+    rotate: {x:0, y:0, z:0}, 
+    delay: 500, 
+});
+
+ScrollReveal().reveal('#text6', {
+    duration: 1500,     
+    origin: 'bottom',   
+    distance: '50px',   
+    scale: 0.2,         
+    opacity: 0,         
+    easing: 'cubic-bezier(0.6, 0.4, 0.1, 1)',   
+    rotate: {x:0, y:0, z:0}, 
+    delay: 500, 
+});
+
