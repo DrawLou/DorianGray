1f7130d79fafab00b4f0dbdcc60cf6eb25c39c7d
DrawLou <74010593+DrawLou@users.noreply.github.com>
2020-12-13 19:31:47 +0100

    scrollreveal

--- Site Web Dorian Gray/scrollreveal.js
+++ Site Web Dorian Gray/scrollreveal.js
@@ -10,7 +10,33 @@ $(function() {
         });
         
     // section RESUME
-    sr.reveal('.livre', {delay: 200, origin: 'top', distance: '100px'});
+    sr.reveal('.livre', {delay: 200, origin: 'bottom', distance: '100px'});
+
+    sr.reveal('.extrait1-1', {delay: 200, origin: 'bottom', distance: '100px'});
+    sr.reveal('.extrait1-2', {delay: 400, origin: 'bottom', distance: '100px'});
+
+    sr.reveal('.extrait2-1', {delay: 200, origin: 'bottom', distance: '100px'});
+    sr.reveal('.extrait2-2', {delay: 400, origin: 'bottom', distance: '100px'});
+
+    sr.reveal('.extrait3-1', {delay: 200, origin: 'bottom', distance: '100px'});
+    sr.reveal('.extrait3-2', {delay: 400, origin: 'bottom', distance: '100px'});
     
+    sr.reveal('.extrait4-1', {delay: 200, origin: 'bottom', distance: '100px'});
+    sr.reveal('.extrait4-2', {delay: 400, origin: 'bottom', distance: '100px'});
+
+    sr.reveal('.extrait5-1', {delay: 200, origin: 'bottom', distance: '100px'});
+    sr.reveal('.extrait5-2', {delay: 400, origin: 'bottom', distance: '100px'});
         
+    sr.reveal('.extrait6-1', {delay: 200, origin: 'bottom', distance: '100px'});
+    sr.reveal('.extrait6-2', {delay: 400, origin: 'bottom', distance: '100px'});
+
+    sr.reveal('p', {delay: 200});
+
+    sr.reveal('#blocImage', {delay: 400});
+
+    sr.reveal('.portrait3 h2', {delay: 200});
+
+    sr.reveal('.portrait4 h2', {delay: 200});
+
+
     });
