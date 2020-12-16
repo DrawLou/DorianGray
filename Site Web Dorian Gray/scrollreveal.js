c3aa276e938519dc298817eed80fdecab4cadf67
DrawLou <74010593+DrawLou@users.noreply.github.com>
2020-12-16 20:30:51 +0100

    css mot / portrait / portrait h2

--- Site Web Dorian Gray/scrollreveal.js
+++ Site Web Dorian Gray/scrollreveal.js
@@ -16,20 +16,12 @@ $(function() {
 
     sr.reveal('.livre', {delay: 200, origin: 'bottom', distance: '100px'});
 
-
-    sr.reveal('.extrait1-1', {delay: 200, origin: 'left', distance: '100px'});
-    sr.reveal('.extrait1-2', {delay: 500, origin: 'right', distance: '100px'});
-
     sr.reveal('.mot1-1', {delay: 200, origin: 'left', distance: '500px'});
     sr.reveal('.mot1-2', {delay: 600, origin: 'right', distance: '500px'});
     sr.reveal('.mot1-3', {delay: 1000, origin: 'left', distance: '500px'});
     sr.reveal('.mot1-4', {delay: 1400, origin: 'right', distance: '500px'});
     sr.reveal('.mot1-5', {delay: 1800, origin: 'left', distance: '500px'});
     sr.reveal('.mot1-6', {delay: 2200, origin: 'right', distance: '500px'});
-
-
-    sr.reveal('.extrait2-1', {delay: 200, origin: 'left', distance: '100px'});
-    sr.reveal('.extrait2-2', {delay: 500, origin: 'right', distance: '100px'});
 
     sr.reveal('.mot2-1', {delay: 200, origin: 'left', distance: '500px'});
     sr.reveal('.mot2-2', {delay: 600, origin: 'right', distance: '500px'});
@@ -38,31 +30,18 @@ $(function() {
     sr.reveal('.mot2-5', {delay: 1800, origin: 'left', distance: '500px'});
     sr.reveal('.mot2-6', {delay: 2200, origin: 'right', distance: '500px'});
 
-
-    sr.reveal('.extrait3-1', {delay: 200, origin: 'left', distance: '100px'});
-    sr.reveal('.extrait3-2', {delay: 500, origin: 'right', distance: '100px'});
-
     sr.reveal('.mot3-1', {delay: 200, origin: 'left', distance: '500px'});
     sr.reveal('.mot3-2', {delay: 600, origin: 'right', distance: '500px'});
     sr.reveal('.mot3-3', {delay: 1000, origin: 'left', distance: '500px'});
     sr.reveal('.mot3-4', {delay: 1400, origin: 'right', distance: '500px'});
     sr.reveal('.mot3-5', {delay: 1800, origin: 'left', distance: '500px'});
     sr.reveal('.mot3-6', {delay: 2200, origin: 'right', distance: '500px'});
-    
-
-    sr.reveal('.extrait4-1', {delay: 200, origin: 'left', distance: '100px'});
-    sr.reveal('.extrait4-2', {delay: 500, origin: 'right', distance: '100px'});
 
     sr.reveal('.mot4-1', {delay: 200, origin: 'left', distance: '500px'});
     sr.reveal('.mot4-2', {delay: 600, origin: 'right', distance: '500px'});
     sr.reveal('.mot4-3', {delay: 1000, origin: 'left', distance: '500px'});
     sr.reveal('.mot4-4', {delay: 1400, origin: 'right', distance: '500px'});
     sr.reveal('.mot4-5', {delay: 1800, origin: 'left', distance: '500px'});
-    sr.reveal('.mot4-6', {delay: 2200, origin: 'right', distance: '500px'});
-
-
-    sr.reveal('.extrait5-1', {delay: 200, origin: 'left', distance: '100px'});
-    sr.reveal('.extrait5-2', {delay: 500, origin: 'right', distance: '100px'});
 
     sr.reveal('.mot5-1', {delay: 200, origin: 'left', distance: '500px'});
     sr.reveal('.mot5-2', {delay: 600, origin: 'right', distance: '500px'});
@@ -70,10 +49,6 @@ $(function() {
     sr.reveal('.mot5-4', {delay: 1400, origin: 'right', distance: '500px'});
     sr.reveal('.mot5-5', {delay: 1800, origin: 'left', distance: '500px'});
     sr.reveal('.mot5-6', {delay: 2200, origin: 'right', distance: '500px'});
-        
-
-    sr.reveal('.extrait6-1', {delay: 200, origin: 'left', distance: '100px'});
-    sr.reveal('.extrait6-2', {delay: 500, origin: 'right', distance: '100px'});
 
     sr.reveal('.mot6-1', {delay: 200, origin: 'left', distance: '500px'});
     sr.reveal('.mot6-2', {delay: 600, origin: 'right', distance: '500px'});
@@ -82,18 +57,13 @@ $(function() {
     sr.reveal('.mot6-5', {delay: 1800, origin: 'left', distance: '500px'});
     sr.reveal('.mot6-6', {delay: 2200, origin: 'right', distance: '500px'});
 
-
     sr.reveal('p', {delay: 200});
-
 
     sr.reveal('#blocImage', {delay: 300});
 
-
     sr.reveal('.portrait2 h2', {delay: 200});
 
-
     sr.reveal('.portrait3 h2', {delay: 200});
-
 
     sr.reveal('.portrait4 h2', {delay: 200});
 
